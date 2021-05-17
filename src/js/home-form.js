4abcc9f5d34d97674d46b7605ce17033d80db98f
Eeveeee <fx8300gfe@gmail.com>
2021-05-17 17:24:14 +0300

    Home - complete, Delivery - complete, Privacy - complete, About - complete, Contacts - complete, 404 - complete

--- src/js/home-form.js
+++
@@ -1,8 +0,0 @@
-const checkboxLabel = document.querySelector('.phone-form-checkbox-wrapper')
-const checkbox = checkboxLabel.querySelector('.checkbox')
-const checkboxFake = checkboxLabel.querySelector('.checkbox-fake')
-
-checkboxLabel.addEventListener('click', (e) => {
-  e.preventDefault()
-  checkboxFake.classList.toggle('active')
-})
